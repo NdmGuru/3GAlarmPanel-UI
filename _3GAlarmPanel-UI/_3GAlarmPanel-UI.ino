049b4c5098456caee4db51a82cc2323d78d8a19e
Nathan Metcalf <ndm@ndm.guru>
2017-12-18 00:02:47 +1100

    Print line not just print...

--- _3GAlarmPanel-UI/_3GAlarmPanel-UI.ino
+++ _3GAlarmPanel-UI/_3GAlarmPanel-UI.ino
@@ -135,7 +135,7 @@ void clearEeprom(){
    for (int i = 0 ; i < EEPROM.length() ; i++) {
     EEPROM.write(i, 0);
   }
-  Serial.print(F("eepromCleared"));
+  Serial.println(F("eepromCleared"));
 }
 
 void imei(){
