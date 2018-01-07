9477ae6f6d7c8b17af66ccea1ace2b025bf1c82f
NdmGuru <ndm@ndm.guru>
2018-01-08 02:49:14 +1100

    Added modded libraries

--- libraries/Adafruit_FONA/Adafruit_FONA.h
+++ libraries/Adafruit_FONA/Adafruit_FONA.h
@@ -21,8 +21,6 @@
 #include "includes/FONAExtIncludes.h"
 #include "includes/platform/FONAPlatform.h"
 
-
-
 #define FONA800L 1
 #define FONA800H 6
 
@@ -38,31 +36,8 @@
 #define FONA_HEADSETAUDIO 0
 #define FONA_EXTAUDIO 1
 
-#define FONA_STTONE_DIALTONE 1
-#define FONA_STTONE_BUSY 2
-#define FONA_STTONE_CONGESTION 3
-#define FONA_STTONE_PATHACK 4
-#define FONA_STTONE_DROPPED 5
-#define FONA_STTONE_ERROR 6
-#define FONA_STTONE_CALLWAIT 7
-#define FONA_STTONE_RINGING 8
-#define FONA_STTONE_BEEP 16
-#define FONA_STTONE_POSTONE 17
-#define FONA_STTONE_ERRTONE 18
-#define FONA_STTONE_INDIANDIALTONE 19
-#define FONA_STTONE_USADIALTONE 20
 
 #define FONA_DEFAULT_TIMEOUT_MS 500
-
-#define FONA_HTTP_GET   0
-#define FONA_HTTP_POST  1
-#define FONA_HTTP_HEAD  2
-
-#define FONA_CALL_READY 0
-#define FONA_CALL_FAILED 1
-#define FONA_CALL_UNKNOWN 2
-#define FONA_CALL_RINGING 3
-#define FONA_CALL_INPROGRESS 4
 
 class Adafruit_FONA : public FONAStreamType {
  public:
@@ -98,21 +73,6 @@ class Adafruit_FONA : public FONAStreamType {
   // IMEI
   uint8_t getIMEI(char *imei);
 
-  // set Audio output
-  boolean setAudio(uint8_t a);
-  boolean setVolume(uint8_t i);
-  uint8_t getVolume(void);
-  boolean playToolkitTone(uint8_t t, uint16_t len);
-  boolean setMicVolume(uint8_t a, uint8_t level);
-  boolean playDTMF(char tone);
-
-  // FM radio functions.
-  boolean tuneFMradio(uint16_t station);
-  boolean FMradio(boolean onoff, uint8_t a = FONA_HEADSETAUDIO);
-  boolean setFMVolume(uint8_t i);
-  int8_t getFMVolume();
-  int8_t getFMSignalLevel(uint16_t station);
-
   // SMS handling
   boolean setSMSInterrupt(uint8_t i);
   uint8_t getSMSInterrupt(void);
@@ -127,62 +87,6 @@ class Adafruit_FONA : public FONAStreamType {
   boolean enableNetworkTimeSync(boolean onoff);
   boolean enableNTPTimeSync(boolean onoff, FONAFlashStringPtr ntpserver=0);
   boolean getTime(char *buff, uint16_t maxlen);
-
-  // GPRS handling
-  boolean enableGPRS(boolean onoff);
-  uint8_t GPRSstate(void);
-  boolean getGSMLoc(uint16_t *replycode, char *buff, uint16_t maxlen);
-  boolean getGSMLoc(float *lat, float *lon);
-  void setGPRSNetworkSettings(FONAFlashStringPtr apn, FONAFlashStringPtr username=0, FONAFlashStringPtr password=0);
-
-  // GPS handling
-  boolean enableGPS(boolean onoff);
-  int8_t GPSstatus(void);
-  uint8_t getGPS(uint8_t arg, char *buffer, uint8_t maxbuff);
-  boolean getGPS(float *lat, float *lon, float *speed_kph=0, float *heading=0, float *altitude=0);
-  boolean enableGPSNMEA(uint8_t nmea);
-
-  // TCP raw connections
-  boolean TCPconnect(char *server, uint16_t port);
-  boolean TCPclose(void);
-  boolean TCPconnected(void);
-  boolean TCPsend(char *packet, uint8_t len);
-  uint16_t TCPavailable(void);
-  uint16_t TCPread(uint8_t *buff, uint8_t len);
-
-  // HTTP low level interface (maps directly to SIM800 commands).
-  boolean HTTP_init();
-  boolean HTTP_term();
-  void HTTP_para_start(FONAFlashStringPtr parameter, boolean quoted = true);
-  boolean HTTP_para_end(boolean quoted = true);
-  boolean HTTP_para(FONAFlashStringPtr parameter, const char *value);
-  boolean HTTP_para(FONAFlashStringPtr parameter, FONAFlashStringPtr value);
-  boolean HTTP_para(FONAFlashStringPtr parameter, int32_t value);
-  boolean HTTP_data(uint32_t size, uint32_t maxTime=10000);
-  boolean HTTP_action(uint8_t method, uint16_t *status, uint16_t *datalen, int32_t timeout = 10000);
-  boolean HTTP_readall(uint16_t *datalen);
-  boolean HTTP_ssl(boolean onoff);
-
-  // HTTP high level interface (easier to use, less flexible).
-  boolean HTTP_GET_start(char *url, uint16_t *status, uint16_t *datalen);
-  void HTTP_GET_end(void);
-  boolean HTTP_POST_start(char *url, FONAFlashStringPtr contenttype, const uint8_t *postdata, uint16_t postdatalen,  uint16_t *status, uint16_t *datalen);
-  void HTTP_POST_end(void);
-  void setUserAgent(FONAFlashStringPtr useragent);
-
-  // HTTPS
-  void setHTTPSRedirect(boolean onoff);
-
-  // PWM (buzzer)
-  boolean setPWM(uint16_t period, uint8_t duty = 50);
-
-  // Phone calls
-  boolean callPhone(char *phonenum);
-  uint8_t getCallStatus(void);
-  boolean hangUp(void);
-  boolean pickUp(void);
-  boolean callerIdNotification(boolean enable, uint8_t interrupt = 0);
-  boolean incomingCallNumber(char* phonenum);
 
   // Helper functions to verify responses.
   boolean expectReply(FONAFlashStringPtr reply, uint16_t timeout = 10000);
